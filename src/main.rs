afba65abb3979f0102558d1e165666da08309090
Jaider R <jaiderrendon66@gmail.com>
2026-02-19 16:44:21 -0500

    Estructura del proyecto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// main.rs
+
+// Orquestador de Bot de Telegram para Scraping de Precio del Cafe de Colombia
+//https://federaciondecafeteros.org/wp/publicaciones/
+
 fn main() {
     println!("Hello, world!");
 }
